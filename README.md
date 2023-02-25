1a9f9a7172fba2a069f1ee9929d2a559f8e222a0
andres-info <diegotlaga@hotmail.es>
2023-02-24 23:50:49 -0600

    Add the keyword Jarvis to start python app

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# Talk to GPT-3-Jarvis 🤖
+Use OpenAI Whisper and text-davinci-003 to talk to an artificial intelligence, is necessary say "Jarvis" for start to use this app
+
+# Inspiring in -> https://github.com/davila7/talk-to-gpt-3#talk-to-gpt-3-
+## Running Locally
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/andres-info/talk-to-gpt-3-jarvis
+
+cd talk-to-gpt-3-jarvis
+```
+2. Install dependencies
+
+These dependencies are required to install with the requirements.txt file:
+
+* pytest 
+* openai 
+* pyttsx3 
+* SpeechRecognition 
+* pyaudio
+* openai whisper 
+
+```bash
+pip install -r requirements.txt
+```
+1. Run
+
+```bash
+python app.py
+```
---
+++ main.py
@@ -0,0 +1,84 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+import whisper
+import json 
+
+# openai
+openai.api_key = proceess.env.OPENAI_API_KEY
+model = whisper.load_model('base')
+engine = pyttsx3.init()
+
+# set engine
+# MX =  com.apple.speech.synthesis.voice.juan
+# US = com.apple.speech.synthesis.voice.Alex
+engine.setProperty('voice', 'com.apple.speech.synthesis.voice.juan')
+
+r = sr.Recognizer()
+mic = sr.Microphone(device_index=0)
+
+# Set keyword to activate listening
+keyword = "Jarvis"
+
+# Set threshold level (low value for low sensitivity, high value for high sensitivity)
+threshold = 500
+
+conversation = ""
+user_name = "Andres"
+bot_name = "Bot"
+
+listening = False
+
+with mic as source:
+    r.adjust_for_ambient_noise(source, duration=0.5)
+
+while True:
+    if not listening:
+        with mic as source:
+            print("Say '{}' to start listening...".format(keyword))
+            audio = r.listen(source)
+
+        try:
+            user_input = r.recognize_google(audio, language="en-US")
+            if user_input.lower() == keyword.lower():
+                listening = True
+                print("Listening...")
+                engine.say("Say something...")
+                engine.runAndWait()
+        except sr.UnknownValueError:
+            continue
+
+    with mic as source:
+        audio = r.listen(source)
+        with open('output.wav', 'wb') as f:
+            f.write(audio.get_wav_data())
+    
+    try:
+        output = model.transcribe('output.wav', fp16=False)
+        user_input = output['text']
+        print("user input : " + user_input)
+    except Exception as e :
+        print(e)
+        continue
+    
+    prompt = user_name + ": " + user_input + "\n" + bot_name+ ": "
+    
+    conversation += prompt
+    
+    response = openai.Completion.create(
+            engine = "text-davinci-003",
+            prompt = conversation,
+            max_tokens = 1024,
+            n = 1,
+            stop=[user_name+":"],
+            temperature=0.2,
+        )
+    response_str = response["choices"][0]["text"].replace("\n", "")
+    response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
+    
+    conversation += response_str + "\n"
+    print(response_str)
+    
+    engine.say(response_str)
+    engine.runAndWait()
+
